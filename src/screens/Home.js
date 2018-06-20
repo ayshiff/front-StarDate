0ce35f85eb6a5d3af202e5d6fdd6718c58a22187
ayshiff <remi.d45@gmail.com>
2018-06-20 13:50:10 +0200

    Initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Page, Toolbar, Tab, Tabbar } from 'react-onsenui';
+import '../App.css';
+
+
+class Home extends Component {
+
+  render() {
+    return (
+        <Page>
+
+          <p>
+              Home page
+          </p>
+
+      </Page>
+    );
+  }
+}
+
+export default Home;
---
+++ src/screens/Profile.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Page, Toolbar, Tab, Tabbar } from 'react-onsenui';
+import '../App.css';
+
+
+class Profile extends Component {
+
+  render() {
+    return (
+        <Page>
+
+          <p>
+              Profile page
+          </p>
+
+      </Page>
+    );
+  }
+}
+
+export default Profile;
