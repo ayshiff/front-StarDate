5e70d4b5a66b26a29d8cfc521c99710cf7806b51
tomdoro <thomas_051998@outlook.fr>
2018-06-27 16:42:22 +0200

    Inte ChatDesktop 🚀

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Page} from 'react-onsenui';
 import '../style/Chat.css';
+import '../style/Home.css';
 import profilePic from '../icons/profilePic.png';
 import * as firebase from 'firebase'
 import _ from 'lodash';
@@ -80,14 +81,14 @@ class Chat extends Component {
                 <svg onClick={this.disconnect} className="Chat_arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"/></svg>
                 <form>
                     {messageNodes}
-        <textarea
-            className="textarea"
-            placeholder="Type a message"
-            cols="100"
-            onChange={this.onChange}
-            onKeyUp={this.onKeyup.bind(this)}
-            value={this.state.message}>
-          </textarea>
+                    <textarea
+                    className="textarea"
+                    placeholder="Type a message"
+                    cols="100"
+                    onChange={this.onChange}
+                    onKeyUp={this.onKeyup.bind(this)}
+                    value={this.state.message}>
+                    </textarea>
                 </form>
             </Page>
         )
