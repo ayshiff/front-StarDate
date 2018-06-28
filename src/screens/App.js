61f3c51545743510e4a06ad2acb02335d3dc9412
ayshiff <remi.d45@gmail.com>
2018-06-28 14:55:05 +0200

    Put api key in config.js

--- src/screens/App.js
+++ src/screens/App.js
@@ -6,20 +6,13 @@ import { Navigator } from 'react-onsenui';
 import MediaQuery from 'react-responsive';
 import * as firebase from 'firebase'
 import Entry from './Entry';
-
+import config from '../config';
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        var config = {
-            apiKey: "AIzaSyBPdT5YJUKo7YK6syefbwUfeCIEbYTPjLM",
-            authDomain: "stardate-4dde0.firebaseapp.com",
-            databaseURL: "https://stardate-4dde0.firebaseio.com",
-            projectId: "stardate-4dde0",
-            storageBucket: "stardate-4dde0.appspot.com",
-            messagingSenderId: "833648798423"
-        };
+
         firebase.initializeApp(config);
     }
 
